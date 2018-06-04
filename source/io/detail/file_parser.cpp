6d3c52a6301ccd25f6ce74987419a6808155faa0
Hou In Si Tou <git@hist.io>
2018-06-04 04:33:04 -0700

    Implemented file_stream with unit tests.
    file_strewam is a wrapper around stream_parser specialized at
    parsing a file in a br archive. It does magic parsing, determines
    the endianness of the file, parses file meta in header such as the
    version number and header size.
    
    Pushes a scope of offset base in stream_parser at construction
    time and pops at destruction time. Seeks the stream position to
    the end of file.

---
+++ source/io/detail/file_parser.cpp
@@ -0,0 +1,59 @@
+#include "file_parser.h"
+#include "integrity_error.h"
+#include <cinttypes>    // for uint32_t
+#include <algorithm>    // for generate
+
+using namespace brtools::io::detail;
+using namespace std;
+
+file_parser::file_parser(stream_parser& sp, const string::size_type magic_length)
+: _m_sp(sp)
+, _m_magic(magic_length, '\0')
+{
+    const auto start_of_file = _m_sp.tell();
+    _m_sp.push_offset_base(start_of_file);
+
+    // populates the file magic
+    generate(_m_magic.begin(), _m_magic.end(), bind(&stream_parser::read<char>, &sp));
+
+    // checks byte order mark, reverses byte order if needed
+    if (0xFEFF != _m_sp.read<uint16_t>())
+    {
+        _m_reverse_bytes_on_destruct = true;
+        _m_sp.reverse_byte_order();
+    }
+
+    _m_sp >> _m_version_number;
+    _m_sp >> _m_size;
+    _m_sp >> _m_header_size;
+}
+
+file_parser::~file_parser()
+{
+    _m_sp.seek_by_offset_from_base(_m_size);
+    _m_sp.pop_offset_base();
+    if (_m_reverse_bytes_on_destruct)
+    {
+        _m_sp.reverse_byte_order();
+    }
+}
+
+string file_parser::file_magic() const
+{
+    return _m_magic;
+}
+
+file_parser::version_num_type file_parser::file_version() const
+{
+    return _m_version_number;
+}
+
+file_parser::header_size_type file_parser::file_header_size() const
+{
+    return _m_header_size;
+}
+
+file_parser::file_size_type file_parser::file_size() const
+{
+    return _m_size;
+}
---
+++ test/tests/file_parser_test.cpp
@@ -0,0 +1,102 @@
+#include <gtest/gtest.h>
+#include <io/detail/stream_parser.h>
+#include <io/detail/file_parser.h>
+
+#include <sstream>  // for istringstream
+
+using namespace ::testing;
+using namespace std;
+using namespace brtools::io::detail;
+
+/**
+ * Tests that a well-formed big endian file is read correctly.
+ * Information in the file should be extracted correctly.
+ */
+TEST(file_parser, big_endian_file)
+{
+    const auto STM_CONTENT = "MAGC"             // file magic
+                             "\xFE\xFF"         // byte order mark, file is big endian
+                             "\xDE\xAD"          // file version
+                             "\x00\x00\x00\x14" // size of file = 20 bytes
+                             "\x00\x0E"         // size of header = 14 bytes, header ends here
+                             "HELLO!"           // actual file content
+                             "B"                // content after file, should be read after file_parser destructs
+                             ""s;
+    istringstream stm(STM_CONTENT);
+    stream_parser sp(stm);
+
+    {
+        file_parser fp(sp);
+        EXPECT_EQ("MAGC"s, fp.file_magic());
+        EXPECT_EQ(0xDEAD, fp.file_version());
+        EXPECT_EQ(0x000E, fp.file_header_size());
+        EXPECT_EQ(0x00000014, fp.file_size());
+        EXPECT_EQ('H', sp.read<char>());
+    }
+
+    // content after file
+    EXPECT_EQ('B', sp.read<char>());
+}
+
+/**
+ * Tests that a well-formed little endian file is read correctly.
+ * Information in the file should be extracted correctly.
+ */
+TEST(file_parser, 5_byte_magic_little_endian_file)
+{
+    const auto STM_CONTENT = "MAGIC"            // file magic
+                             "\xFF\xFE"         // byte order mark, file is little endian
+                             "\xAD\xDE"         // file version
+                             "\x14\x00\x00\x00" // size of file = 20 bytes
+                             "\x0F\x00"         // size of header = 15 bytes, header ends here
+                             "HELLO"            // actual file content
+                             "B"                // content after file, should be read after file_parser destructs
+                             ""s;
+    istringstream stm(STM_CONTENT);
+    stream_parser sp(stm);
+
+    {
+        file_parser fp(sp, 5);
+        EXPECT_EQ("MAGIC"s, fp.file_magic());
+        EXPECT_EQ(0xDEAD, fp.file_version());
+        EXPECT_EQ(0x000F, fp.file_header_size());
+        EXPECT_EQ(0x00000014, fp.file_size());
+        EXPECT_EQ('H', sp.read<char>());
+    }
+
+    // content after file
+    EXPECT_EQ('B', sp.read<char>());
+}
+
+/**
+ * Tests byte order setting is restored after file_parser destructed.
+ */
+TEST(file_parser, byte_order_restored_after_completed_read)
+{
+    const auto STM_CONTENT = "\xBE\xEF"         // for setting expectation
+
+                             "MAGC"             // file magic
+                             "\xFF\xFE"         // byte order mark, file is little endian
+                             "\xAD\xDE"         // file version
+                             "\x0E\x00\x00\x00" // size of file = 14 bytes
+                             "\x0E\x00"         // size of header = 14 bytes, header and file ends here
+
+                             "\xBE\xEF"         // same 2 bytes as beginning of stream
+
+                             "MAGC"             // file magic
+                             "\xFE\xFF"         // byte order mark, file is big endian
+                             "\xDE\xAD"         // file version
+                             "\x00\x00\x00\x0E" // size of file = 14 bytes
+                             "\x00\x0E"         // size of header = 14 bytes, header and file ends here
+
+                             "\xBE\xEF"         // same 2 bytes as beginning of stream
+                             ""s;
+    istringstream stm(STM_CONTENT);
+    stream_parser sp(stm);
+
+    const auto expected = sp.read<uint16_t>();
+    file_parser{sp};
+    EXPECT_EQ(expected, sp.read<uint16_t>());
+    file_parser{sp};
+    EXPECT_EQ(expected, sp.read<uint16_t>());
+}
